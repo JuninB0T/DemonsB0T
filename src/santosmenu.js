85292f234370ba7fd8208a522a61f6f04866ce16
JuninB0T <juninreportbot@gmail.com>
2021-02-11 22:21:22 -0300

    First commit

---
+++ src/santosmenu.js
@@ -0,0 +1,14 @@
+const santosmenu = (prefix, pushname) => {
+  return `◪ *Comandos do Santos*
+ │
+ │    _Obs somente Santos pode usar esses comando_
+ │
+ ├─ ❏ ${prefix}setprefix
+ ├─ ❏ ${prefix}block
+ ├─ ❏ ${prefix}bc
+ ├─ ❏ ${prefix}bcgc
+ └─ ❏ ${prefix}clearall`
+
+}
+
+exports.santosmenu = santosmenu
